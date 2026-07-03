86b4122a7db8c4915505d8df9483c5f159fba5a5
takrdh32-byte <takrdh32@gmail.com>
2026-07-03 23:45:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'theme/app_theme.dart';
-import 'l10n/app_strings.dart';
+import 'locale.dart';                 // ✅ Changed from l10n/app_strings.dart
 import 'screens/home_screen.dart';
 import 'services/storage_service.dart';
 import 'services/notification_service.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await StorageService.init();
   await NotificationService.init();
   runApp(const DawaCheckApp());
 }
 
-class DawaCheckApp extends StatefulWidget {
+class DawaCheckApp extends StatelessWidget {
   const DawaCheckApp({super.key});
 
   @override
-  State<DawaCheckApp> createState() => _DawaCheckAppState();
-}
-
-class _DawaCheckAppState extends State<DawaCheckApp> {
-  @override
-  void initState() {
-    super.initState();
-    LocaleController.instance.addListener(_onLocaleChange);
-  }
-
-  @override
-  void dispose() {
-    LocaleController.instance.removeListener(_onLocaleChange);
-    super.dispose();
-  }
-
-  void _onLocaleChange() => setState(() {});
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DawaCheck',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      home: const HomeScreen(),
+    return ChangeNotifierProvider(
+      create: (_) => LocaleController.instance,
+      child: Consumer<LocaleController>(
+        builder: (ctx, locale, child) {
+          return MaterialApp(
+            title: S.of('app_name'),
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.light,
+            locale: locale.lang == AppLang.en
+                ? const Locale('en')
+                : const Locale('hi'),
+            supportedLocales: const [Locale('en'), Locale('hi')],
+            home: const HomeScreen(),
+          );
+        },
+      ),
     );
   }
 }
